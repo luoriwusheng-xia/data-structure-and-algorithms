872f9b65c5bbf19a1d2e6b4216bd6f4c90e1c3ee
夏麒麟 <1197802519@qq.com>
2023-11-21 13:54:18 +0800

    feat: 优化面试题-进阶-文档结构，按照章节分篇幅，减少一页巨大的DOM

---
+++ .vitepress/sidebar.js
@@ -0,0 +1,216 @@
+export default {
+  // '/docs/frontend/': [
+  //   {
+  //     text: '前端',
+  //     items: [
+  //       {
+  //         text: '杂文',
+  //         link: '/docs/frontend/index'
+  //       }
+  //     ]
+  //   }
+  // ],
+  "/docs/frontend/translation/index": [
+    {
+      text: 'codemirror-中文文档',
+      link: '/docs/frontend/translation/codemirror/index'
+    }
+  ],
+  '/docs/list/': [
+    {
+      text: 'leetcode',
+      items: [
+        { text: '链表', link: '/docs/list/01/index' },
+        { text: '数组', link: '/docs/list/02/index' },
+        { text: '树', link: '/docs/list/03/index' },
+        { text: '排序', link: '/docs/list/04/index' },
+        { text: '字符串', link: '/docs/list/05/index' },
+        { text: '动态规划', link: '/docs/list/06/index' },
+        { text: '位运算', link: '/docs/list/07/index' },
+        { text: '双指针', link: '/docs/list/08/index' },
+        { text: '素数个数统计', link: '/docs/list/09/index' },
+        { text: '数学', link: '/docs/list/10/index' },
+
+        {
+          text: '简单',
+          items: [
+            {
+              text: '回文数',
+              link: '/docs/list/11/index'
+            }
+          ]
+        },
+        {
+          text: '中等',
+          items: [
+            {
+              text: '寻找两个正序数组的中位数',
+              link: '/docs/list/12/index'
+            }
+          ]
+        }
+      ],
+    },
+  ],
+
+  '/docs/vitepress-config/': [
+    {
+      text: 'vitepress',
+      items: [
+        {
+          text: '评论功能',
+          link: '/docs/vitepress-config/01/index',
+        },
+        {
+          text: '多语言',
+          link: '/docs/vitepress-config/02/index',
+        },
+        {
+          text: '主题开发',
+          link: '/docs/vitepress-config/03/index',
+        },
+        {
+          text: 'element-plus接入',
+          link: '/docs/vitepress-config/04/index',
+        },
+        {
+          text: 'sandpack接入',
+          link: '/docs/vitepress-config/05/index',
+        },
+        {
+          text: '使用unocss',
+          link: '/docs/vitepress-config/06/index',
+        },
+        {
+          text: '使用vue',
+          link: '/docs/vitepress-config/07/index',
+        },
+        {
+          text: '编程式使用 Router 和 Route',
+          link: '/docs/vitepress-config/08/index',
+        },
+        {
+          text: '插件开发',
+          link: '/docs/vitepress-config/01/index',
+        },
+        {
+          text: '全局搜索',
+          link: '/docs/vitepress-config/01/index',
+        },
+        {
+          text: '部署',
+          link: '/docs/vitepress-config/01/index',
+        },
+      ],
+    },
+  ],
+
+  '/docs/backend/file/': [
+    {
+      text: '文件分片上传',
+      link: '/docs/backend/file/01'
+    },
+    {
+      text: '文件分片下载',
+      link: '/docs/backend/file/02'
+    }
+  ],
+
+  '/docs/backend/jwt/': [
+    {
+      text: 'jwt登录',
+      link: '/docs/backend/jwt/'
+    },
+    {
+      text: 'RBAC',
+      link: '/docs/backend/jwt/rbac'
+    }
+  ],
+
+  '/docs/backend/optimize/01': [
+    {
+      text: '压测',
+      link: '/docs/backend/optimize/01'
+    }
+  ],
+  '/docs/backend/websocket/01': [
+    {
+      text: 'websocket',
+      link: '/docs/backend/websocket/01'
+    }
+  ],
+
+  '/docs/frontend/essay/': [
+    {
+      text: '渡一前端',
+      link: '/docs/frontend/essay/01/',
+      items: [
+        {
+          text: '判断函数是否标记了async',
+          link: '/docs/frontend/essay/01/'
+        },
+        {
+          text: '消除异步的传染性',
+          link: '/docs/frontend/essay/02/'
+        },
+        {
+          text: '自动检测更新',
+          link: '/docs/frontend/essay/03/'
+        },
+        {
+          text: '妙用位运算-算法-找唯一数字',
+          link: '/docs/frontend/essay/04/'
+        }
+      ]
+    }
+  ],
+  '/docs/vue2/': [
+    {
+      text: '源码首页',
+      link: '/docs/vue2/'
+    },
+    {
+      text: '01-',
+      link: '/docs/vue2/01'
+    },
+    {
+      text: '02-',
+      link: '/docs/vue2/02'
+    },
+    {
+      text: '03-响应式原理',
+      link: '/docs/vue2/03'
+    },
+    {
+      text: '04-异步更新',
+      link: '/docs/vue2/04'
+    },
+    {
+      text: '05-全局API实现原理',
+      link: '/docs/vue2/05'
+    },
+    {
+      text: '2.7.14 调试',
+      link: '/docs/vue2/30'
+    },
+    {
+      text: '变更日志',
+      link: '/docs/vue2/changelog'
+    }
+  ],
+
+  '/docs/interview/improve/': [
+    {
+      text: '一、js基础',
+      link: '/docs/interview/improve/01'
+    },
+    {
+      text: '二、HTML',
+      link: '/docs/interview/improve/02'
+    },
+    {
+      text: '三、css',
+      link: '/docs/interview/improve/02'
+    }
+  ]
+}
